8f0509132536931454bdab1582421ddc0c5bff0f
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-14 13:38:25 -0500

    changed name

---
+++ javascripts/departmentDom.js
@@ -0,0 +1,21 @@
+const events = require('./events');
+const outputDiv = document.getElementById('departments');
+
+const domString = (departmentArray) => {
+  let domString = '';
+  departmentArray.forEach((department) => {
+    domString += `<div class="col-sm-3 department">`;
+    domString +=   `<h3 class="hide department-title" data-department-id="${department.id}">${department.name}</h3>`;
+    domString +=   `<img class="department-image"src=${department.img}>`;
+    domString += `</div>`;
+  });
+  return domString;
+};
+
+const printDepartmentToDom = (departmentArray) => {
+  outputDiv.innerHTML = domString(departmentArray);
+  console.log(events);
+  events.addDepartmentEvents();
+};
+
+module.exports = printDepartmentToDom;
