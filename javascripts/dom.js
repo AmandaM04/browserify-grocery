163e82814a267cc33e09d7da1f7424125a7e4d9d
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-14 13:17:56 -0500

    called events

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,10 +1,11 @@
+const events = require('./events');
 const outputDiv = document.getElementById('departments');
 
 const domString = (departmentArray) => {
   let domString = '';
   departmentArray.forEach((department) => {
     domString += `<div class="col-sm-3 department">`;
-    domString +=   `<h3 class="hide">${department.name}</h3>`;
+    domString +=   `<h3 class="hide department-title" data-department-id="${department.id}">${department.name}</h3>`;
     domString +=   `<img class="department-image"src=${department.img}>`;
     domString += `</div>`;
   });
@@ -13,6 +14,8 @@ const domString = (departmentArray) => {
 
 const printToDom = (departmentArray) => {
   outputDiv.innerHTML = domString(departmentArray);
+  console.log(events);
+  events.addDepartmentEvents();
 };
 
 module.exports = printToDom;
