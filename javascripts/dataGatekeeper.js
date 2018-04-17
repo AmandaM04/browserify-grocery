59a20705b738cb8fc57fbdbb73714784a2faf736
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-16 19:46:46 -0500

    wip

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -5,7 +5,6 @@ const data = require('./data');
 
 const showItemsOnLoad = function () {
   const itemsData = JSON.parse(this.responseText).items;
-  console.log('itemsData', itemsData);
   data.setItems(itemsData);
 };
 
